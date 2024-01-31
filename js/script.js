36a4d6e366622e5a0c0bfc5adec39e436bf452c6
Eras <erasylserikov36@gmail.com>
2024-01-31 16:23:15 +0600

    scroll rewiews added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+(function() {
+
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.querySelector('.rewiews__cards').scrollLeft -= (delta*100);
+        e.preventDefault();
+    }
+    if (document.querySelector('.rewiews__cards').addEventListener) {
+        document.querySelector('.rewiews__cards').addEventListener("mousewheel", scrollHorizontally, false);
+        document.querySelector('.rewiews__cards').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        document.querySelector('.rewiews__cards').attachEvent("onmousewheel", scrollHorizontally);
+    }
+
+})();
